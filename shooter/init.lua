9131f82df82f9b3823af13fecb1aae623bef250b
BrunoMine <borgesdossantosbruno@gmail.com>
2017-07-23 13:27:49 -0300

    Separate flaregun mod

--- shooter/init.lua
+++ shooter/init.lua
@@ -2,9 +2,7 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 dofile(modpath.."/api.lua")
 
-if SHOOTER_ENABLE_FLARES == true then
-	dofile(modpath.."/flaregun.lua")
-end
+
 if SHOOTER_ENABLE_HOOK == true then
 	dofile(modpath.."/grapple.lua")
 end
