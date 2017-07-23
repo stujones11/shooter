90302d2637645b49663edacafefa330b44453d94
BrunoMine <borgesdossantosbruno@gmail.com>
2017-07-23 13:44:26 -0300

    Separate hook mod

--- shooter/init.lua
+++ shooter/init.lua
@@ -3,9 +3,5 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 dofile(modpath.."/api.lua")
 
 
-if SHOOTER_ENABLE_HOOK == true then
-	dofile(modpath.."/grapple.lua")
-end
-
 
 
