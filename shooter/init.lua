a68a75f78fe6b2d2d136d4728ac61b30e137fefd
stujones11 <stujones111@gmail.com>
2018-01-03 20:24:36 +0000

    Auto remove outdated turret entities

--- shooter/init.lua
+++ shooter/init.lua
@@ -2,6 +2,10 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 dofile(modpath.."/api.lua")
 
-
-
-
+minetest.register_entity("shooter:turret_entity", {
+	visual = "sprite",
+	textures = {"blank.png"},
+	on_activate = function(self)
+		self.object:remove()
+	end,
+})
