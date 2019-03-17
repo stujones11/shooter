a88e0222e3631216f0d617cee48aed1c920eaa06
stujones11 <stujones111@gmail.com>
2019-03-17 18:13:26 +0000

    Update license and include source headers

--- shooter_rocket/init.lua
+++ shooter_rocket/init.lua
@@ -1,3 +1,22 @@
+/*
+Shooter Rocket Gun [shooter_rocket]
+Copyright (C) 2013-2019 stujones11, Stuart Jones
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 minetest.register_craftitem("shooter_rocket:rocket", {
 	description = "Rocket",
     stack_max = 1,
